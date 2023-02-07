f9785856911ce2c00f7e2b69ab702f099073cb5b
Cynthia Pottin <36019063+cpottin@users.noreply.github.com>
2023-02-07 14:08:54 -0500

    added timer AB#629

---
+++ src/main/java/Timer.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+public class Timer implements Runnable {
+
+    private static long startTime = System.currentTimeMillis();
+    private static long elapsedTime = 0L;
+
+//    public static void timerSet(){
+//        System.out.println("You have 7 minutes to complete the game.");
+//
+//        if(elapsedTime < (60 * 1000)) {
+//            elapsedTime = (new Date()).getTime() - startTime;
+//        }
+//        else
+//            System.out.println("You ran out of time...Game Over");
+//        }
+//    }
+
+    @Override
+    public void run() {
+        System.out.println("You have 7 minutes to complete the game.");
+
+        while (elapsedTime < (7*60 * 1000)) {
+            elapsedTime = (new Date()).getTime() - startTime;
+        }
+        if (elapsedTime == (7*60 * 1000)) {
+            System.out.println("==================================");
+            System.out.println("You ran out of time...Game Over");
+            System.out.println("==================================");
+//            gameClient.setQuitGame(true);
+            GameClientUtil.gameExitMessage();
+            System.exit(0);
+        }
+    }
+
+
+}
+
