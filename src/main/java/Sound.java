8a8043273dbee4d9a20fef9db2a072a5915a3f40
Bryce Meadors <bryce.meadors@gmail.com>
2023-02-10 14:56:31 -0800

    Lowered music volume, added mute functionality, AB#47

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -2,18 +2,34 @@ import javax.sound.sampled.*;
 import java.io.IOException;
 
 public class Sound {
-
-
-    public void playSound(){
-        String soundFile = "Medieval_game.wav";
-        try{
-            AudioInputStream ais = AudioSystem.getAudioInputStream(this.getClass().getResource(soundFile));
-            Clip clip = AudioSystem.getClip();
-            clip.open(ais);
-            clip.start();
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        }catch (UnsupportedAudioFileException | IOException | LineUnavailableException e){
-
+    private String soundFile;
+    private Clip clip;
+    public void playSound() {
+        setSoundFile("Medieval_game.wav");
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(this.getClass().getResource(getSoundFile()));
+            setClip(AudioSystem.getClip());
+            getClip().open(ais);
+            getClip().start();
+            getClip().loop(Clip.LOOP_CONTINUOUSLY);
+            final FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(-15.0f);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e){
         }
+    }
+    public void setSoundFile(String soundFile) {
+        this.soundFile = soundFile;
+    }
+
+    public String getSoundFile() {
+        return this.soundFile;
+    }
+
+    public void setClip(Clip clip) {
+        this.clip = clip;
+    }
+
+    public Clip getClip() {
+        return this.clip;
     }
 }
